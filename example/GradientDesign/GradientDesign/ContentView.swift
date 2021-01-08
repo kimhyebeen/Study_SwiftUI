3f7c3a87a045e29a0b5aff5c545984aa4f7fb0bf
kimhyebeen <kimhbin@naver.com>
2021-01-09 01:26:10 +0900

    D. 프로젝트 생성

---
+++ example/GradientDesign/GradientDesign/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GradientDesign
+//
+//  Created by 김혜빈 on 2021/01/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ example/GradientDesign/GradientDesign/GradientDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientDesignApp.swift
+//  GradientDesign
+//
+//  Created by 김혜빈 on 2021/01/09.
+//
+
+import SwiftUI
+
+@main
+struct GradientDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
